94cc3978b091a45207f5a64a5ee04dc2e8494ca5
Liana Mancini <liana.mancini@gmail.com>
2018-01-17 13:20:21 -0500

    binary agents

--- algos.js
+++ algos.js
@@ -4,6 +4,26 @@
 // Free Code Camp Algorithm Challenges - lianagitsit
 
 
+/* BINARY AGENTS: Return a translated sentence from the passed binary string.
+
+function binaryAgent(str) {
+  var myArray = str.split(" ");
+  var strArray = [];
+  var final;
+  
+  for (var i = 0; i < myArray.length; i++){
+    strArray.push(String.fromCharCode(parseInt(myArray[i], 2)));
+  }
+  
+  final = strArray.join("");
+  
+  return final;
+}
+
+binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
+
+
+
 /*STEAMROLLER: Flatten an array of indeterminate levels of nesting
 
 function steamrollArray(arr) {
